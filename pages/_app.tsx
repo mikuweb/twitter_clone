e0d480ca096cc83976dd97916a3d420dc50d0f1f
mikuweb <mxxx.kwtmg@gmail.com>
2023-05-04 10:00:07 +0900

    create onToggle function to switch to LoginModal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import Layout from "@/components/Layout";
 import LoginModal from "@/components/modals/LoginModal";
+import RegisterModal from "@/components/modals/RegisterModal";
 // import Modal from "@/components/Modal";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       {/* <Modal actionLabel="Submit" isOpen title="Test Modal" /> */}
       <LoginModal />
+      <RegisterModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>
